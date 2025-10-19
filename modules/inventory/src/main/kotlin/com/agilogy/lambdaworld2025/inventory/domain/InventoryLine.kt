53953735dbcd773b75e3c36b50356516566bf416
Jordi Pradel <jordi.pradel@agilogy.com>
2025-10-19 23:45:15 +0200

    Implement reconcileStock with raise DSL

--- modules/inventory/src/main/kotlin/com/agilogy/lambdaworld2025/inventory/domain/InventoryLine.kt
+++ modules/inventory/src/main/kotlin/com/agilogy/lambdaworld2025/inventory/domain/InventoryLine.kt
@@ -1,7 +1,6 @@
 package com.agilogy.lambdaworld2025.inventory.domain
 
-import arrow.core.Either
-import arrow.core.raise.either
+import arrow.core.raise.Raise
 import kotlin.time.Instant
 
 @ConsistentCopyVisibility
@@ -9,13 +8,13 @@ data class InventoryLine
 private constructor(val sku: String, val stock: Int, val reconciliationDate: Instant) {
 
     companion object {
-        operator fun invoke(
+        operator fun Raise<IllegalStockAmountNegative>.invoke(
             sku: String,
             stock: Int,
             reconciliationDate: Instant,
-        ): Either<IllegalStockAmountNegative, InventoryLine> = either {
+        ): InventoryLine {
             if (stock < 0) raise(IllegalStockAmountNegative(stock))
-            InventoryLine(sku, stock, reconciliationDate)
+            return InventoryLine(sku, stock, reconciliationDate)
         }
     }
 }
