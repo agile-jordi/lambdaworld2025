f40ab6afed958ebd5db7248452b16b53434c8655
Jordi Pradel <jordi.pradel@agilogy.com>
2025-10-22 08:16:02 +0200

    Implement reconcileStock with either DSL and error accumulation

--- modules/inventory/src/main/kotlin/com/agilogy/lambdaworld2025/inventory/domain/InventoryService.kt
+++ modules/inventory/src/main/kotlin/com/agilogy/lambdaworld2025/inventory/domain/InventoryService.kt
@@ -1,9 +1,14 @@
+@file:OptIn(ExperimentalRaiseAccumulateApi::class)
+
 package com.agilogy.lambdaworld2025.inventory.domain
 
 import arrow.core.Either
+import arrow.core.NonEmptyList
 import arrow.core.getOrElse
+import arrow.core.raise.ExperimentalRaiseAccumulateApi
+import arrow.core.raise.RaiseAccumulate
+import arrow.core.raise.accumulate
 import arrow.core.raise.either
-import arrow.core.raise.recover
 import kotlin.time.Instant
 
 class InventoryService(
@@ -15,7 +20,7 @@ class InventoryService(
         sku: String,
         stock: Int,
         reconciliationDate: Instant,
-    ): Either<ReconcileStockError, InventoryLine> = either {
+    ): Either<NonEmptyList<ReconcileStockError>, InventoryLine> = eitherAccumulate {
         val currentStock =
             recover(inventoryRepository.getCurrentStock(sku)) {
                 productsRepository.registerProduct(sku).bind()
@@ -23,12 +28,20 @@ class InventoryService(
             }
 
         if (currentStock != null && currentStock.reconciliationDate >= reconciliationDate) {
-            raise(IllegalReconciliationDateEarlierThanLast(currentStock))
+            accumulateError(IllegalReconciliationDateEarlierThanLast(currentStock))
         }
-        val line = InventoryLine(sku, stock, reconciliationDate).bind()
-        inventoryRepository.register(line).bind()
-        line
+        val line = InventoryLine(sku, stock, reconciliationDate).bindOrAccumulate()
+        inventoryRepository.register(line.value).bind()
+        line.value
     }
 }
 
 fun <E, A> recover(e: Either<E, A>, f: (E) -> A): A = e.getOrElse(f)
+
+fun <E, A> eitherAccumulate(f: RaiseAccumulate<E>.() -> A): Either<NonEmptyList<E>, A> = either {
+    accumulate(f)
+}
+
+fun <E> RaiseAccumulate<E>.accumulateError(e: E): RaiseAccumulate.Value<Nothing> = accumulating {
+    raise(e)
+}
